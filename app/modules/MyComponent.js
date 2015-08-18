a72b3e4bdc362db8eb628823d2412010171c40d4
nicohexun <nico_hexun@163.com>
2015-08-19 00:05:59 +0800

    step 2

---
+++ app/modules/MyComponent.js
@@ -0,0 +1,8 @@
+import './MyComponent.css'
+import React from 'react';
+
+export default class Hello extends React.Component {
+    render() {
+        return <div className = "MyComponent-wrapper" > < h1 > Hello world < /h1></div > ;
+    }
+}
