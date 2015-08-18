51926bb11c0f3d8b8994e20ce885c94273a9c4f9
nicohexun <nico_hexun@163.com>
2015-08-18 17:00:08 +0800

    init

---
+++ app/modules/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Hello extends React.createClass({
+  render: function () {
+    return (
+      <div className="MyComponent-wrapper">
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
+});
